b9625bb65d9c3c7290915b50a43f0933fcf6ea3d
aman khavse <161040988+Amankhavse@users.noreply.github.com>
2026-04-22 09:33:53 +0530

    Add files via upload

---
+++ aman profile card/JavaScript.js
@@ -0,0 +1,14 @@
+﻿<script>
+    const toggleBtn = document.getElementById("theme-toggle");
+
+    toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+    // icon change
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+        } else {
+        toggleBtn.textContent = "🌙";
+        }
+    });
+</script>
